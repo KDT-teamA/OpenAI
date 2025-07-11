6571f048902d42d06ab50ac22e57bc3fa0165ba5
tatoon0 <qudwjd4296@naver.com>
2025-07-11 13:05:23 +0900

    commit

--- src/main/java/com/example/chatgpt/Controller/OpenAiRestController.java
+++ src/main/java/com/example/chatgpt/Controller/OpenAiRestController.java
@@ -1,11 +1,14 @@
 package com.example.chatgpt.Controller;
 
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.chatgpt.Service.OpenAiService;
 
@@ -20,5 +23,11 @@ public class OpenAiRestController {
     @PostMapping("/query")
     public @ResponseBody ChatResponse query(@RequestParam(defaultValue = "인기있는 여행지는", name = "query") String prompt) {
         return openAiService.answerQuery(prompt);
+    }
+    
+    @PostMapping(path = "/image", consumes= MediaType.MULTIPART_FORM_DATA_VALUE)
+    public @ResponseBody ChatResponse explainImage(@RequestParam(defaultValue = "이 이미지는 무엇인가요?", name = "prompt") String prompt,
+                                                   @RequestPart(required = true, name = "image" ) MultipartFile image) {
+        return openAiService.explainTheImage(prompt, image.getResource());
     }
 }
--- src/main/java/com/example/chatgpt/Service/OpenAiService.java
+++ src/main/java/com/example/chatgpt/Service/OpenAiService.java
@@ -7,18 +7,36 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.model.Media;
 import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.openai.api.OpenAiApi.ChatCompletion;
+import org.springframework.ai.openai.api.OpenAiApi.ChatCompletionMessage;
+import org.springframework.ai.openai.api.OpenAiApi.ChatCompletionRequest;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MimeTypeUtils;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class OpenAiService {
     
-    @Autowired
-    OpenAiChatModel openAiChatModel;
-    // @Autowired
-    // OpenAiApi openAiApi;
+    private final OpenAiChatModel openAiChatModel;
+    private final OpenAiApi openAiApi;
+
+    public ResponseEntity<ChatCompletion> chatResponse(String query) {
+        ChatCompletionMessage systemMsg = new OpenAiApi.ChatCompletionMessage("안녕하세요, 챗봇입니다.", ChatCompletionMessage.Role.SYSTEM);
+        ChatCompletionMessage usermsg1 = new OpenAiApi.ChatCompletionMessage("안녕 봇", ChatCompletionMessage.Role.USER);
+        ChatCompletionMessage assistantMsg1 = new OpenAiApi.ChatCompletionMessage("환영합니다.", ChatCompletionMessage.Role.SYSTEM);
+        ChatCompletionMessage userMsg2 = new OpenAiApi.ChatCompletionMessage(query, ChatCompletionMessage.Role.USER);
+
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest(List.of(systemMsg, usermsg1, assistantMsg1, userMsg2),
+                                                                                OpenAiApi.ChatModel.GPT_4_O.getValue(),
+                                                                                0.8,
+                                                                                false);
+        return openAiApi.chatCompletionEntity(chatCompletionRequest);
+    }
 
     // 일반 응답
     public ChatResponse answerQuery(String prompt) {
